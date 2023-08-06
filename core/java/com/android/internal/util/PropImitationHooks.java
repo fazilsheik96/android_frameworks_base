c2e995d1fc284b49b41200a413dd83207768c8f5
someone5678 <nemui3353@gmail.com>
2023-08-06 19:29:27 +0900

    PropImitationHooks: Set HARDWARE, ID values
    * Google apps also check these values
    
    Change-Id: I846986121faec39d3ece044794fd0f459e8d435d

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -70,9 +70,11 @@ public class PropImitationHooks {
     private static final Map<String, String> sPixelEightProps = Map.of(
         "PRODUCT", "husky",
         "DEVICE", "husky",
+        "HARDWARE", "husky",
         "MANUFACTURER", "Google",
         "BRAND", "google",
         "MODEL", "Pixel 8 Pro",
+        "ID", "AP1A.240505.005",
         "FINGERPRINT", "google/husky/husky:14/AP1A.240505.005/11677807:user/release-keys"
     );
 
