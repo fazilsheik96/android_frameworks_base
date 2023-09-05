9d09c5710b1fa2156fa3b52e696527656b19c46d
minaripenguin <minaripenguin@users.noreply.github.com>
2023-09-05 08:01:59 +0800

    VolumeDialogImpl: Add sound effects for volume adjustments
    Change-Id: Ibd41d59279261bf2bd2f2cdeedeffc8c8f44a2c5
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -77,6 +77,7 @@ import android.graphics.drawable.RotateDrawable;
 import android.media.AppVolume;
 import android.media.AudioManager;
 import android.media.AudioSystem;
+import android.media.MediaPlayer;
 import android.media.session.MediaController;
 import android.media.session.MediaSessionManager;
 import android.media.session.PlaybackState;
@@ -348,6 +349,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     private int customVolumeStyles = 0;
 
+    private MediaPlayer mediaPlayer = null;
+
     private boolean mShowMediaController = true;
 
     @VisibleForTesting
@@ -452,6 +455,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         mTunerService.addTunable(mTunable, CUSTOM_VOLUME_STYLES);
 
         initDimens();
+        initMediaPlayer();
 
         mOrientation = mContext.getResources().getConfiguration().orientation;
         mDevicePostureController = devicePostureController;
@@ -3151,6 +3155,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                             userLevel);
                 }
             }
+            playSound();
         }
 
         @Override
@@ -3171,6 +3176,27 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                 mHandler.sendMessageDelayed(mHandler.obtainMessage(H.RECHECK, mRow),
                         USER_ATTEMPT_GRACE_PERIOD);
             }
+        }
+    }
+
+    private void initMediaPlayer() {
+        try {
+            mediaPlayer = MediaPlayer.create(mContext, R.raw.volume_control_sound);
+        } catch (Exception e) {
+            Log.d(TAG, "Error initializing media player");
+        }
+    }
+
+    private void playSound() {
+        AudioManager audioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        if (audioManager == null || audioManager.isMusicActive() || mediaPlayer == null) {
+            return;
+        }
+        try {
+            mediaPlayer.seekTo(0);
+            mediaPlayer.start();
+        } catch (Exception e) {
+            Log.d(TAG, "Error playing sound effect");
         }
     }
 
