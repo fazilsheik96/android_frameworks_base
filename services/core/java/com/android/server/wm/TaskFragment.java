c7ee7b9921a89707162dc4906ecf5e9a1a636931
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-27 11:36:22 +0800

    services: Implement exit app animation boost
    original change by: mnri
    
    committer: rmp
    
    pre-approved by: mnri
    
    Change-Id: Ie99c49530695bb0af6e0d8c5f715f3244cb6997a
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/wm/TaskFragment.java
+++ services/core/java/com/android/server/wm/TaskFragment.java
@@ -85,9 +85,11 @@ import android.content.res.Configuration;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.hardware.HardwareBuffer;
+import android.hardware.power.Boost;
 import android.os.IBinder;
 import android.os.PowerManagerInternal.PowerExtBoosts;
 import android.os.RemoteException;
+import android.os.PowerManagerInternal;
 import android.os.UserHandle;
 import android.util.BoostFramework;
 import android.util.DisplayMetrics;
@@ -105,6 +107,7 @@ import android.window.TaskFragmentOrganizerToken;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.app.ActivityTrigger;
 import com.android.internal.protolog.common.ProtoLog;
+import com.android.server.LocalServices;
 import com.android.server.am.HostingRecord;
 import com.android.server.pm.pkg.AndroidPackage;
 
@@ -1424,6 +1427,7 @@ class TaskFragment extends WindowContainer<WindowContainer> {
                 // Exit app animation boost
                 if (next != null && mPerf != null) {
                     mPerf.perfHint(BoostFramework.VENDOR_HINT_EXIT_ANIM_BOOST, next.packageName);
+                    doActivityBoost();
                 }
             }
         }
@@ -1595,6 +1599,13 @@ class TaskFragment extends WindowContainer<WindowContainer> {
         }
 
         return true;
+    }
+
+    protected void doActivityBoost() {
+        PowerManagerInternal mPowerManagerInternal = LocalServices.getService(PowerManagerInternal.class);
+        if (mPowerManagerInternal != null) {
+            mPowerManagerInternal.setPowerBoost(Boost.DISPLAY_UPDATE_IMMINENT, 80);
+        }
     }
 
     boolean shouldSleepOrShutDownActivities() {
