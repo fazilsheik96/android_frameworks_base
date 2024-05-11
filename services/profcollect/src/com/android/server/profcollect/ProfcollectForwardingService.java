23d4f21944601ac33ca04071c8837f205651fb5b
Yi Kong <yikong@google.com>
2024-05-11 09:07:55 +0800

    profcollectd: call jobFinished even if failed
    Test: presubmit
    Google: 3081089
    Change-Id: I4472fcf8140a8a9bceaff6b3c5a7abf6bc8df69e

--- services/profcollect/src/com/android/server/profcollect/ProfcollectForwardingService.java
+++ services/profcollect/src/com/android/server/profcollect/ProfcollectForwardingService.java
@@ -246,6 +246,7 @@ public final class ProfcollectForwardingService extends SystemService {
                                     + e.getMessage());
                         }
                     });
+            jobFinished(params, false);
             return true;
         }
 
