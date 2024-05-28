96359cb48f915733096c17d0dc4f9bd3e5155408
fazilsheik96 <fazil.sheik96@gmail.com>
2024-05-28 14:15:26 -0400

    SettingsLib: Simplify the code used to show Deep sleep
    idoybh (YAAP):
    Rewrite it better, more readable, get rid of useless conversions etc
    
    Change-Id: Ia81c99edc09ece3f3cb8d62c049ba99b113286f9
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: I99a0736fb14c004e3f61285c5739bc77ad82bf53

--- packages/SettingsLib/src/com/android/settingslib/deviceinfo/AbstractUptimePreferenceController.java
+++ packages/SettingsLib/src/com/android/settingslib/deviceinfo/AbstractUptimePreferenceController.java
@@ -90,14 +90,15 @@ public abstract class AbstractUptimePreferenceController extends AbstractPrefere
     }
 
     private void updateTimes() {
-        float deepSleepRatio = Math.max((float) (SystemClock.elapsedRealtime() - SystemClock.uptimeMillis()), 0f)
-                / SystemClock.elapsedRealtime();
-        int deepSleepPercent = Math.round(deepSleepRatio * 100);
-
-        String summary = DateUtils.formatElapsedTime(SystemClock.elapsedRealtime() / 1000) +
-                " " + mContext.getString(R.string.status_deep_sleep, deepSleepPercent, "%");
-
-        mUptime.setSummary(summary.toString());
+        final long realtime = SystemClock.elapsedRealtime();
+        final long uptime = SystemClock.uptimeMillis();
+        final float deepSleepTime = Math.max((float) (realtime - uptime), 0f);
+        final int deepSleepPercent = Math.round((deepSleepTime / realtime) * 100f);
+        final String formattedElapsed = DateUtils.formatElapsedTime(realtime / 1000);
+        final String formattedDeepSleep =
+                mContext.getString(R.string.status_deep_sleep, deepSleepPercent, "%");
+        final String summary = formattedElapsed + " " + formattedDeepSleep;
+        mUptime.setSummary(summary);
     }
 
     private static class MyHandler extends Handler {
