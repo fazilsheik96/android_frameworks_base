NAAP-again/frameworks_base

base: Make weather provider a tri-state setting [1/2]
Also improve the code, add docs and backup the values

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -166,5 +166,8 @@ public class SystemSettings {
         Settings.System.MAX_MUSIC_VOLUME,
         Settings.System.MAX_ALARM_VOLUME,
         Settings.System.FULLSCREEN_GESTURES,
+        Settings.System.LOCKSCREEN_WEATHER_PROVIDER,
+        Settings.System.LOCKSCREEN_WEATHER_LOCATION,
+        Settings.System.LOCKSCREEN_WEATHER_TEXT,
     };
 }

fazilsheik96/android_frameworks_base

Change-Id: I7d39d91205ba07b6186947297650e7efb17c12d4

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -108,6 +108,9 @@ public class SystemSettings {
         Settings.System.NETWORK_TRAFFIC_STATE,
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
         Settings.System.OMNI_ADVANCED_REBOOT,
-        Settings.System.BACK_GESTURE_HEIGHT
+        Settings.System.BACK_GESTURE_HEIGHT,
+        Settings.System.LOCKSCREEN_WEATHER_PROVIDER,
+        Settings.System.LOCKSCREEN_WEATHER_LOCATION,
+        Settings.System.LOCKSCREEN_WEATHER_TEXT
     };
 }