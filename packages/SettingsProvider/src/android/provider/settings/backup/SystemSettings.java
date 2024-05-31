yaap/frameworks_base

base: Add click actions for OmniJaws [1/2]
when the user taps on the keyguard view
together with a setting whether to update or launch the app

Also make sure we unregister the settings observer in the view when we can

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -168,6 +168,7 @@ public class SystemSettings {
         Settings.System.LOCKSCREEN_WEATHER_PROVIDER,
         Settings.System.LOCKSCREEN_WEATHER_LOCATION,
         Settings.System.LOCKSCREEN_WEATHER_TEXT,
+        Settings.System.LOCKSCREEN_WEATHER_CLICK_UPDATES,
         Settings.System.QS_WIFI_AUTO_ON,
         Settings.System.QS_BT_AUTO_ON,
     };

fazilsheik96/android_frameworks_base

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -111,6 +111,7 @@ public class SystemSettings {
         Settings.System.BACK_GESTURE_HEIGHT,
         Settings.System.LOCKSCREEN_WEATHER_PROVIDER,
         Settings.System.LOCKSCREEN_WEATHER_LOCATION,
-        Settings.System.LOCKSCREEN_WEATHER_TEXT
+        Settings.System.LOCKSCREEN_WEATHER_TEXT,
+        Settings.System.LOCKSCREEN_WEATHER_CLICK_UPDATES
     };
 }